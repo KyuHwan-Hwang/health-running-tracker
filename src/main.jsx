47c65426aa54ff9cd70667d264d1cc0b2e703a27
황규환 <kyuhwan.hwang@aroaduser.com>
2025-03-22 23:46:57 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import App from './App';
 import HealthRunningTracker from './HealthRunningTracker';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/tracker' element={<HealthRunningTracker />} />
       </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 );
